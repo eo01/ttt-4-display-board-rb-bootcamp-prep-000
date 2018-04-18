cfcdb455161a0eea88ba94213fd9b45c34866732
eo 01 <eo_01@protonmail.com>
2018-04-18 15:39:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "   |   |   "
+  puts " #{0} | #{1} | #{2} "
   puts "-----------" 
-  puts "   |   |   "
+  puts " #{3} | #{4} | #{5} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{6} | #{7} | #{8} "
 end
 board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
